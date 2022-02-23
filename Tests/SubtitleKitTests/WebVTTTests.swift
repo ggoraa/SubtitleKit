695543288a322da623f16d51d14025b480825759
GGorAA <ggoraa1029@gmail.com>
2022-02-23 21:36:15 +0200

    refactor: tests

--- Tests/SubtitleKitTests/WebVTTTests.swift
+++ Tests/SubtitleKitTests/WebVTTTests.swift
@@ -12,7 +12,6 @@ import XCTest
 class WebVTTTests: XCTestCase {
     func testBasicParsing() throws {
         let parsed = try WebVTTParser(content: "WEBVTT - Этот файл не содержит реплик.").parse()
-        print(parsed)
         XCTAssert(parsed.name == "Этот файл не содержит реплик.")
         XCTAssert(parsed.body.isEmpty)
     }
@@ -35,8 +34,6 @@ WEBVTT - Этот файл содержит реплики.
 - [ Визг летучих мышей ]
 - Они не попадут в твои волосы. They're after the bugs.
 """).parse()
-        print(parsed)
         XCTAssert(parsed.name == "Этот файл содержит реплики.")
-//        XCTAssert(parsed.body.isEmpty)
     }
 }
